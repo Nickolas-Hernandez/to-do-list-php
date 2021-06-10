4df65c616ab17cb89af04624ab0120e90348244a
Nick Hernandez <xnickhernandez@gmail.com>
2021-06-10 17:17:46 +0000

    Completed delete dfeature

--- server/src/get-todos.php
+++ server/src/get-todos.php
@@ -4,4 +4,9 @@ require_once "db.php";
 
 $result = mysqli_query($db, "SELECT * FROM to_dos", MYSQLI_USE_RESULT);
 
+if(mysqli_error($db)){
+  echo "Unable to access database" . mysqli_error($db);
+  exit;
+}
+
 ?>
